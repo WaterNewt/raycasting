742c2d2ea7781c1ef217e015e176e44deec5f9c7
WaterNewt <134024849+WaterNewt@users.noreply.github.com>
2024-03-23 09:01:02 +0500

    Improved setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
+extensions = [
+    Extension("walls", ["walls.pyx"]),
+    Extension("ray", ["ray.pyx"])
+]
+
 setup(
-    ext_modules=cythonize(["walls.pyx", "ray.pyx"])
+    author='Yunus Ruzmetov',
+    license='GPL-3.0',
+    ext_modules=cythonize(extensions),
+    python_requires='>=3.6',
+    install_requires=[
+        "pygame",
+        "cython"
+    ]
 )
